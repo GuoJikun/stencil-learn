4731adfc131cb84a74c45d62d517b2e862a8d138
GuoJikun <guojikun486546@163.com>
2022-12-03 14:56:25 +0800

    feat: 生命周期演示

--- src/components.d.ts
+++ src/components.d.ts
@@ -21,6 +21,7 @@ export namespace Components {
         "middle": string;
     }
     interface WebText {
+        "text": string;
     }
 }
 declare global {
@@ -57,6 +58,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface WebText {
+        "text"?: string;
     }
     interface IntrinsicElements {
         "my-component": MyComponent;
--- src/components/web-text/web-text.tsx
+++ src/components/web-text/web-text.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop, Watch } from '@stencil/core';
 
 @Component({
   tag: 'web-text',
@@ -6,13 +6,52 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class WebText {
+  @Prop() text: string;
 
-  render() {
-    return (
-      <Host>
-        <slot></slot>
-      </Host>
-    );
+  @Watch('text')
+  handlerTextWatcher(val: string, oldVal: string) {
+    console.log('新值：', val, '，旧值：', oldVal);
+    console.log('生命周期：watch');
   }
 
+  connectedCallback() {
+    console.log('生命周期：connectedCallback');
+  }
+
+  componentWillLoad() {
+    console.log('生命周期：componentWillLoad');
+  }
+
+  componentWillRender() {
+    console.log('生命周期：componentWillRender');
+  }
+
+  render() {
+    console.log('生命周期：render');
+    return <Host>{this.text}</Host>;
+  }
+
+  componentDidRender() {
+    console.log('生命周期：componentDidRender');
+  }
+
+  componentDidLoad() {
+    console.log('生命周期：componentDidLoad');
+  }
+
+  componentShouldUpdate() {
+    console.log('生命周期：componentShouldUpdate');
+  }
+
+  componentWillUpdate() {
+    console.log('生命周期：componentWillUpdate');
+  }
+
+  componentDidUpdate() {
+    console.log('生命周期：componentDidUpdate');
+  }
+
+  disconnectedCallback() {
+    console.log('生命周期：disconnectedCallback');
+  }
 }
